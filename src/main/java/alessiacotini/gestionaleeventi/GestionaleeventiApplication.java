1f649425c869eb54bebe69b4ff0ee9d594581ef4
AlessiaCotini <alessia.cotini@icloud.com>
2026-07-17 10:08:25 +0200

    verifica connessione al db

--- src/main/java/alessiacotini/gestionaleeventi/GestionaleeventiApplication.java
+++ src/main/java/alessiacotini/gestionaleeventi/GestionaleeventiApplication.java
@@ -8,6 +8,8 @@ public class GestionaleeventiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionaleeventiApplication.class, args);
+
+		//System.out.println("VERIFICO CONNESSIONE AL DB");
 	}
 
 }
