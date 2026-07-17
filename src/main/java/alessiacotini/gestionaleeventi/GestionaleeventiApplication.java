5b12ec6914985d2f660a7045b0de86aebd17c88f
AlessiaCotini <alessia.cotini@icloud.com>
2026-07-17 09:38:21 +0200

    Initial commit

---
+++ src/main/java/alessiacotini/gestionaleeventi/GestionaleeventiApplication.java
@@ -0,0 +1,13 @@
+package alessiacotini.gestionaleeventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleeventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionaleeventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alessiacotini/gestionaleeventi/GestionaleeventiApplicationTests.java
@@ -0,0 +1,13 @@
+package alessiacotini.gestionaleeventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleeventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
